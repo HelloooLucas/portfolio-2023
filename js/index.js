267b136fc53a6f80a848891dc09eed7352a8e998
HelloooLucas <montenoise.lucas@gmail.com>
2023-03-25 17:36:52 +0100

    remove responsive font-size at root-level and implement clamps with CSS-tricks method

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function calculate({
+  minWidth = 340,
+  maxWidth = 1024,
+  minFontSize,
+  maxFontSize,
+}) {
+  const minWidthInRem = minWidth / 16;
+  const maxWidthInRem = maxWidth / 16;
+
+  const minFontSizeInRem = minFontSize / 16;
+  const maxFontSizeInRem = maxFontSize / 16;
+
+  const slope =
+    (maxFontSizeInRem - minFontSizeInRem) / (maxWidthInRem - minWidthInRem);
+  const yAxisIntersection = -minWidthInRem * slope + minFontSizeInRem;
+
+  const preferredValue = `${yAxisIntersection.toFixed(3)}rem + ${(
+    slope * 100
+  ).toFixed(3)}vw`;
+
+  console.log({ preferredValue });
+}
+
+// calculate({
+//   minFontSize: 24,
+//   maxFontSize: 48,
+// });
